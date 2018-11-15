0d9d6ec1934f2293fac0eec1cbbfbd3fb8492a7b
iFdit <lyliyangwc@gmail.com>
2018-11-15 17:50:41 +0800

    format code style

--- config/env.js
+++ config/env.js
@@ -8,14 +8,13 @@ const raw = Object.keys(process.env)
     NODE_ENV: process.env.NODE_ENV || 'development',
   })
 
-  const stringified = {
-    'process.env': Object.keys(raw).reduce((env, key) => {
-      env[key] = JSON.stringify(raw[key])
-      return env
-    }, {}),
-  }
+const stringified = {
+  'process.env': Object.keys(raw).reduce((env, key) => {
+    env[key] = JSON.stringify(raw[key])
+    return env
+  }, {}),
+}
 
-  module.exports = {
-    stringified
-  }
-  
+module.exports = {
+  stringified
+}
